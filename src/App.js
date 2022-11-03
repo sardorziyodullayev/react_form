24c2761e44ef7a3e31683d2d3ed1b1aa7c9cb2ce
sardorziyodullayev <sardorziyodullayev2004@gmail.com>
2022-11-03 14:49:54 +0500

    initialize commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<div className="box">
+				<div className="form">
+					<h2>Welcome back, Olivia</h2>
+					<p className="form-text">Continue with Google or enter your details</p>
+					<a href="" className="google">
+						<img
+							className="google-img"
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJRPM0YKkH8TPMNGEYHITBhn1GIyZfNcIvSw&usqp=CAU"
+							alt=""
+						/>
+						<h5>Log in with Google</h5>
+					</a>
+					<div className="inputBox">
+						<input className="log-input" type="email" />
+						<label>Email</label>
+						<i></i>
+					</div>
+
+					<div className="inputBox">
+						<input className="log-input" type="password" />
+						<label>Password</label>
+						<i></i>
+					</div>
+					<div className="links">
+						<div className="check-box">
+							<input type="checkbox" />
+							<span>Remember for 30 days</span>
+						</div>
+						<a href="#">Forgot Password</a>
+					</div>
+					<button type="submit">Log in</button>
+					<div className="reg-box">
+						<span>Don't have an account?</span>
+						<a href="">Sign up for free</a>
+					</div>
+				</div>
+				<div className="image">
+					<img src="https://picsum.photos/id/60/500/515" alt="" />
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
